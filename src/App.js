d7ad996cf71bd2c624e7b61be424203c8c3c6ab9
Sergey Untilov <s.untilov@gmail.com>
2021-07-19 23:59:31 +0300

    Minor update

--- src/App.js
+++ src/App.js
@@ -15,9 +15,6 @@ function App() {
         <br/>
         <BitNumber caption="Result" />
         <br/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
