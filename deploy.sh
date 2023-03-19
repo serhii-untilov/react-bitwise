e6b7705f28204a3ef31fc9af701ea0d72a3aa5d3
Sergey Untilov <s.untilov@gmail.com>
2023-03-19 03:48:27 +0200

    Deploy, install & apache config

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-user_name=pi
-host_name=st
-rsync -avzP --rsh=ssh ./build/* "$(user_name)"@"$(host_name)":/home/"$(user_name)"/react-bitwise/
-rsync -avzP --rsh=ssh ./install.sh "$(user_name)"@"$(host_name)":/home/"$(user_name)"/react-bitwise/
-rsync -avzP --rsh=ssh ./apache.conf "$(user_name)"@"$(host_name)":/home/"$(user_name)"/react-bitwise/
+userName="pi"
+hostName="st"
+rsync -avzP --rsh=ssh ./build/* $userName@$hostName:/home/$userName/react-bitwise/
+rsync -avzP --rsh=ssh ./install.sh $userName@$hostName:/home/$userName/react-bitwise/
+rsync -avzP --rsh=ssh ./apache.conf $userName@$hostName:/home/$userName/react-bitwise/
