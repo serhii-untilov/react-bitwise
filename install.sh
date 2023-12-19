eab35c139b261b1429a5700e8f67d13bb2db152b
Sergey Untilov <s.untilov@gmail.com>
2023-12-19 16:41:26 +0200

    Installation script

---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo apt install wget
+wget https://raw.githubusercontent.com/serhii-untilov/react-bitwise/master/Dockerfile
+wget https://raw.githubusercontent.com/serhii-untilov/react-bitwise/master/scripts/docker-jenkins.sh
+chmod +x ./docker-jenkins.sh
+./docker-jenkins.sh
