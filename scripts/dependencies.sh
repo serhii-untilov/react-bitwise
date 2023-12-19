9667f8f3df324c25edff9ce81616e50cc4af2cd2
Sergey Untilov <s.untilov@gmail.com>
2023-12-19 10:00:23 +0200

    Install Node script

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,15 +1,15 @@
 #/bin/bash
 NODE_MAJOR=18
-apt install sudo
-usermod -aG sudo "$(whoami)"
-usermod -aG wheel "$(whoami)"
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt install -y ca-certificates curl gnupg
+# apt install sudo
+# usermod -aG sudo "$(whoami)"
+# usermod -aG wheel "$(whoami)"
+apt update -y
+apt upgrade -y
+apt install -y ca-certificates curl gnupg
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /usr/share/keyrings/nodesource.gpg
 echo "deb [signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-sudo apt update -y
-sudo apt install nodejs -y
+apt update -y
+apt install nodejs -y
 node -v
-sudo apt install build-essential -y
+apt install build-essential -y
 npm install
